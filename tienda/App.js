bb60ec5ea6baf719ee78a2dbc6096a16d3198b90
David Liva <davidliva01@gmail.com>
2024-05-12 22:54:08 -0300

    Se ha integrado una api de perros en la app

--- tienda/App.js
+++ tienda/App.js
@@ -1,8 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './views/Home';
-import OtraVentana from './views/OtraVentana';
-
+import OtraVentana from './views/OtraVentana'; 
 
 export default function App() {
   const Stack = createNativeStackNavigator();
--- tienda/views/OtraVentana.jsx
+++ tienda/views/OtraVentana.jsx
@@ -1,119 +1,89 @@
 import { View, Text, Button, FlatList, Image } from "react-native"
 import { ScaledSheet } from "react-native-size-matters"
 import { useNavigation } from '@react-navigation/native';
-import perro1 from '../assets/perros/perro1.jpg'
-import perro2 from '../assets/perros/perro2.jpg'
-import perro3 from '../assets/perros/perro3.jpg'
-import perro4 from '../assets/perros/perro4.jpg'
 import { useEffect, useState } from "react";
 
 
 const OtraVentana = () => {
 
-    const navigation = useNavigation();
 
-    const api = "https://swapi.dev/api/people"
 
-    useEffect(() => {
-        fetch(api)
-        .then((res) => res.json())
-        .then((data) => { setImgPerro(data.results[0])
-            }   
-        )
-    },[])
+// const styles = ScaledSheet.create({
+//     container: {
+//         marginTop: '50@ms',
+//         alignItems:'center'
+//     },
+//     text: {
+//         fontSize: '30@s',
+//         textAlign: 'center',
+//         marginBottom:'10@ms'
+//     },
+//     image: {
+//         width: 200,
+//         height:200
+//     },
+//     imageContainer: {
+//         alignContent:'center',
+//         alignItems:'center',
+//         marginBottom: '10@vs',
+//         backgroundColor: '#039',
+//         paddingHorizontal: 50,
+//         paddingVertical:10,
+//         marginHorizontal: 50,
+//         marginVertical:10,
+//         borderRadius: 10,
+//         opacity:0.8
+//     },
+//     textDescription: {
+//         color: '#fff',
+//         textAlign: 'center',
+//         fontSize: 15
 
-    const [imgPerro, setImgPerro] = useState()
-    const nombres = imgPerro
-    console.log("imagen perro:",nombres)
+//     },
+//     title:{
+//         color: '#fff',
+//         textDecorationLine: 'underline',
+//         textAlign: 'center',
+//         fontSize: 20
+//     }
+// });
 
-    const DATOS = [
-        {
-            nombre: 'Perro Lobo',
-            descripcion:'Blanco con gris, amigable, jugueton, come mucho, duerme en las camas.',
-            img: perro1,
-        },
-        {
-            nombre: 'Perro Peluche',
-            descripcion:'Chiquito, nunca crece, jugueton, necesita mucha atención, mucha comida, mucho amor.',
-            img: perro2,
-        },
-        {
-            nombre: 'Perro Básico',
-            descripcion:'Negro, obediente, miedoso, no come mucho, dale un hueso y puchero y está contento.',
-            img: perro3,
-        },
-        {
-            nombre: 'Perro vaca',
-            descripcion:'2X1 Parecen vacas pero son perros blancos con manchas negras, macho y hembra, pareja.',
-            img: perro4,
-        }
-    ]
+const [images, setImages] = useState([]);
 
-    const Item = ({nombre, descripcion, img}) => {
-        return(
-            <View style={styles.imageContainer}>
-                    <Text style={styles.title} >{nombre}</Text>
-                    <Text style={styles.textDescription} >{descripcion}</Text>
-            <Image style={styles.image} source={img}/>
-        </View>
-        )
+useEffect(() => {
+  const fetchImages = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breed/pug/images');
+      const data = await response.json();
+      setImages(data.message);
+    } catch (error) {
+      console.error('Error fetching images:', error);
     }
+  };
 
-    return(
-        <>
-            <View style={styles.container}>
-                <Text style={styles.text}>
-                    Adoptar Perros
-                </Text>
-                <Button title='Adoptar gatos' onPress={() => navigation.navigate('home')} />
-            </View>
+  fetchImages();
+}, []);
 
-            <FlatList 
-                data={DATOS} 
-                renderItem={({item}) => <Item nombre={nombres.name} descripcion={item.descripcion} img={item.img} />}
-            />
-        </>
-    )
+const renderItem = ({ item }) => (
+    
+  <View>
+    <Image
+    source={{ uri: item }}
+    style={{ width: 200, height: 200, marginBottom: 10 }}
+  />
+  </View>
+);
+
+return (
+  <View>
+    <Text>Adoptar perros</Text>
+    <FlatList
+      data={images}
+      renderItem={renderItem}
+      keyExtractor={(item, index) => index.toString()}
+    />
+  </View>  
+);
 }
 
-
-const styles = ScaledSheet.create({
-    container: {
-        marginTop: '50@ms',
-        alignItems:'center'
-    },
-    text: {
-        fontSize: '30@s',
-        textAlign: 'center',
-        marginBottom:'10@ms'
-    },
-    image: {
-        width: 200,
-        height:200
-    },
-    imageContainer: {
-        alignContent:'center',
-        alignItems:'center',
-        marginBottom: '10@vs',
-        backgroundColor: '#039',
-        paddingHorizontal: 50,
-        paddingVertical:10,
-        marginHorizontal: 50,
-        marginVertical:10,
-        borderRadius: 10,
-        opacity:0.8
-    },
-    textDescription: {
-        color: '#fff',
-        textAlign: 'center',
-        fontSize: 15
-
-    },
-    title:{
-        color: '#fff',
-        textDecorationLine: 'underline',
-        textAlign: 'center',
-        fontSize: 20
-    }
-});
 export default OtraVentana
